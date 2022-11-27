2a369ef60cc6a0a98b69e01593a6254a0d9c49a9
Jeet Dholakia <onemediasoft@gmail.com>
2022-11-27 10:15:46 +0530

    Initial Commit

---
+++ Swifty Twitter/Swifty_TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swifty_TwitterApp.swift
+//  Swifty Twitter
+//
+//  Created by Jeet Dholakia on 27/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Swifty_TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
